409a3c11d4002bd25b5751128b43ef76c2bbac8b
Diego Vaca Paz <diegovacapaz@hotmail.com>
2024-01-29 20:12:31 -0300

    Se hicieron los metodos de cambio de rol

--- controllers/users.controller.js
+++ controllers/users.controller.js
@@ -29,6 +29,62 @@ const getAllUsers = async (req, res) => {
     }
 }
 
+const setAdminTrue = async (req, res) => {
+    try{
+        const user = await userModel.findById(req.params.id);
+        if(!user) return res.status(404).json({ message: "El usuario no existe"});
+        user.isAdmin = true;
+        await user.save();
+        return res.status(200).json({message: `${user.username} ahora es administrador`});
+    }
+    catch(error){
+        console.log(error);
+        res.status(500).json({ message: error.message });
+    }
+}
+
+const setAdminFalse = async (req, res) => {
+    try{
+        const user = await userModel.findById(req.params.id);
+        if(!user) return res.status(404).json({ message: "El usuario no existe"});
+        user.isAdmin = false;
+        await user.save();
+        return res.status(200).json({message: `${user.username} ya no es administrador`});
+    }
+    catch(error){
+        console.log(error);
+        res.status(500).json({ message: error.message });
+    }
+}
+
+const setEditorFalse = async (req, res) => {
+    try{
+        const user = await userModel.findById(req.params.id);
+        if(!user) return res.status(404).json({ message: "El usuario no existe"});
+        user.isEditor = false;
+        await user.save();
+        return res.status(200).json({message: `${user.username} ya no es editor`});
+    }
+    catch(error){
+        console.log(error);
+        res.status(500).json({ message: error.message });
+    }
+}
+
+const setEditorTrue = async (req, res) => {
+    try{
+        const user = await userModel.findById(req.params.id);
+        if(!user) return res.status(404).json({ message: "El usuario no existe"});
+        user.isEditor = true;
+        await user.save();
+        return res.status(200).json({message: `${user.username} ahora es editor`});
+    }
+    catch(error){
+        console.log(error);
+        res.status(500).json({ message: error.message });
+    }
+}
+
 const updateUserInfo = async (req, res) => { //Hay que hacer el sistema de autenticacion antes
     try{
         const user = await userModel.findById(req.params.id);
@@ -46,4 +102,4 @@ const updateUserInfo = async (req, res) => { //Hay que hacer el sistema de auten
     }
 }
 
-module.exports = {getUserById, getAllUsers};
+module.exports = {getUserById, getAllUsers, setAdminFalse, setAdminTrue, setEditorFalse, setEditorTrue};
--- routes/users.routes.js
+++ routes/users.routes.js
@@ -1,9 +1,14 @@
 const express = require('express');
 const usersController = require("../controllers/users.controller.js");
+const {verifyAdmin} = require("../helpers/permissions.validation.js");
 
 const router = express.Router();
 
-router.get("/users", usersController.getAllUsers);
-router.get("/users/:id", usersController.getUserById);
+router.get("/users", verifyAdmin, usersController.getAllUsers);
+router.get("/users/:id", verifyAdmin, usersController.getUserById);
+router.put("/users/setAdminTrue/:id", verifyAdmin, usersController.setAdminTrue);
+router.put("/users/setAdminFalse/:id", verifyAdmin, usersController.setAdminFalse);
+router.put("/users/setEditorTrue/:id", verifyAdmin, usersController.setEditorTrue);
+router.put("/users/setEditorFalse/:id", verifyAdmin, usersController.setEditorFalse);
 
 module.exports = router;
