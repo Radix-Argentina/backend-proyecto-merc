4f0c912dd5b4ee19f77e35d462b57d573b685065
Diego Vaca Paz <diegovacapaz@hotmail.com>
2024-01-31 18:31:47 -0300

    Se movio el archivo de permisos

--- routes/suppliers.routes.js
+++ routes/suppliers.routes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const suppliersController = require("../controllers/suppliers.controller.js");
 
 const router = express.Router();
-
+//Falta ponerle los middlewares de permisos
 router.post("/suppliers", suppliersController.createSupplier);
 router.put("/suppliers/:id", suppliersController.updateSupplier);
 router.delete("/suppliers/:id", suppliersController.deleteSupplier);
