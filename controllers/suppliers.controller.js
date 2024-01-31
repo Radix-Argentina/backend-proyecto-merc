01e242ca71cd3aeace8aac96fe5b8ec33c633060
Diego Vaca Paz <diegovacapaz@hotmail.com>
2024-01-31 18:26:26 -0300

    Se hizo el metodo delete supplier

--- controllers/suppliers.controller.js
+++ controllers/suppliers.controller.js
@@ -29,4 +29,52 @@ const createSupplier = async (req, res) => {
     }
 }
 
-module.exports = { createSupplier};
+const updateSupplier = async (req, res) => {
+    try {
+        const supplier = await supplierModel.findById(req.params.id);
+        if(!supplier) return res.status(404).json({message: "El provedor que desea modificar no existe"});
+
+        if(req.body.name) supplier.name = req.body.name;
+        if(req.body.phone) supplier.phone = req.body.phone;
+        if(req.body.mail) supplier.mail = req.body.mail;
+        if(req.body.address) supplier.address = req.body.address;
+
+        const repeatedName = await supplierModel.findOne({ name: supplier.name });
+        if(repeatedName && !repeatedName._id.equals(supplier._id)) return res.status(400).json({message: "Ya existe un provedor con ese nombre"});
+        
+        if(typeof supplier.mail == "string" && !validations.validateMail(supplier.mail)) return res.status(400).json({message: "Correo inválido"});
+        if(typeof supplier.phone == "string"  && !validations.validatePhone(supplier.phone)) return res.status(400).json({message: "Teléfono inválido"});
+        if(typeof supplier.address == "string" && !validations.validateAddress(supplier.address)) return res.status(400).json({message: "Dirección inválida"});
+
+        await supplier.save();
+
+        return res.status(200).json({
+            supplier,
+            message: "El provedor se modificó con éxito"
+        });
+    }
+    catch(error) {
+        console.log(error);
+        res.status(500).json({message: error.message});
+    }
+}
+
+const deleteSupplier = async (req, res) => {
+    try{
+        const supplier = await supplierModel.findById(req.params.id);
+        if(!supplier) return res.status(404).json({message: "El provedor que desea eliminar no existe"});
+        if(supplier.isActive) return res.status(400).json({message: "Solo puede eliminar provedores inactivos"});
+        
+        await supplierModel.findByIdAndDelete(req.params.id);
+        return res.status(200).json({
+            supplier,
+            message: "El provedor fue eliminado con éxito",
+        });
+    }
+    catch(error){
+        console.log(error);
+        res.status(500).json({message: error.message});
+    }
+}
+
+module.exports = { createSupplier, updateSupplier, deleteSupplier};
--- routes/suppliers.routes.js
+++ routes/suppliers.routes.js
@@ -4,5 +4,7 @@ const suppliersController = require("../controllers/suppliers.controller.js");
 const router = express.Router();
 
 router.post("/suppliers", suppliersController.createSupplier);
+router.put("/suppliers/:id", suppliersController.updateSupplier);
+router.delete("/suppliers/:id", suppliersController.deleteSupplier);
 
 module.exports = router;
